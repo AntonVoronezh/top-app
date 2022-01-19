ec1959e6ea9e0cd669727671b4b9b23040c37d49
AntonVoronezh <pstut@mail.ru>
2022-01-19 18:31:34 +0300

    add component Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import { Htag } from "../components";
+import { Htag } from '../components';
+import { Button } from '../components/Button/Button';
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      <Htag tag={"h1"}>gggg</Htag>
+      <Htag tag={'h1'}>gggg</Htag>
+      <Button appearance={'primary'}> кнопка </Button>
+      <Button appearance={'ghost'}> кнопка </Button>
     </div>
   );
 }
