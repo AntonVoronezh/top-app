e636ee97ffd70dc41c39a4f057083ac62133653e
AntonVoronezh <pstut@mail.ru>
2022-01-20 19:13:55 +0300

    add Rating

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, KeyboardEvent } from 'react';
 
 import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
@@ -15,12 +15,14 @@ export const Rating = ({ isEditable = false, rating, setRating, className, ...pr
   const constructRating = (currentRating: number) => {
     const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
       return (
-        <StarIcon
+        <span
           className={cn(styles.star, { [styles.filled]: i < currentRating, [styles.editable]: isEditable })}
           onMouseEnter={() => changeDisplay(i + 1)}
           onMouseLeave={() => changeDisplay(rating)}
           onClick={() => onClick(i + 1)}
-        />
+        >
+          <StarIcon tabIndex={isEditable ? 0 : -1} onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)} />
+        </span>
       );
     });
 
@@ -37,6 +39,14 @@ export const Rating = ({ isEditable = false, rating, setRating, className, ...pr
 
   const onClick = (i: number) => {
     if (!isEditable || !setRating) {
+      return;
+    }
+
+    setRating(i);
+  };
+
+  const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+    if (e.code !== 'Space' || !setRating) {
       return;
     }
 
