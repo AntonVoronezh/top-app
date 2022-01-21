6339672a02bbeda8f59f3bf43deec59f92279fdf
AntonVoronezh <pstut@mail.ru>
2022-01-21 15:48:19 +0300

    add Footer

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { LayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar/Sidebar';
+import { Header } from './Header/Header';
 import styles from './Layout.module.css';
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,5 +2,5 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}></div>;
+  return <div {...props}>Sidebar</div>;
 };
