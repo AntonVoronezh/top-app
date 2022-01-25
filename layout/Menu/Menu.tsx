6b4e1c2e47a5cdbc869714809adc334ec8c311b6
AntonVoronezh <pstut@mail.ru>
2022-01-25 18:26:02 +0300

    add icons

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -4,6 +4,16 @@ import styles from './Menu.module.css';
 import cn from 'classnames';
 import { format } from 'date-fns';
 import { AppContext } from '../../context/app.context';
+import { FirstLevelMenuItem } from '../../interfaces/menu.interface';
+import CoursesItem from './icons/courses.svg'
+import BooksItem from './icons/books.svg'
+import ProductItem from './icons/product.svg'
+import ServicesItem from './icons/services.svg'
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+  {route: 'courses', name: 'Курсы', icon: }
+]
+
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
