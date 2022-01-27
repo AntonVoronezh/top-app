5aad28ae345bcac96bfa4fcb2c7ad6ea64888313
AntonVoronezh <pstut@mail.ru>
2022-01-27 17:31:57 +0300

    add TopPageComponentProps

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,14 @@
 import styles from './P.module.css';
 import cn from 'classnames';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import { Htag, Tag } from '../../components';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <>
+      <Htag tag='h1'>{page.title}</Htag>
+      {products && <Tag color='grey' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
+      <div>{products && products.length}</div>
+    </>
+  );
 };
