438e44502459b5d71ecd145b026e8a542e77eb3d
AntonVoronezh <pstut@mail.ru>
2022-01-27 18:06:07 +0300

    add hhData

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,31 +1,25 @@
 import styles from './TopPageComponent.module.css';
-import cn from 'classnames';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import { Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { HhData } from '../../components/HhData/HhData';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
-        <Htag tag="h1">{page.title}</Htag>
-        {products && (
-          <Tag color="grey" size="m" aria-label={products.length + 'элементов'}>
-            {products.length}
-          </Tag>
-        )}
-        <span>сортировка</span>
+        <Htag tag='h1'>{page.title}</Htag>
+        {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+        <span>Сортировка</span>
       </div>
-
-      <div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
-
+      <div>
+        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+      </div>
       <div className={styles.hhTitle}>
-        <Htag tag="h2">Вакансии - {page.category}</Htag>
-        {products && (
-          <Tag color="red" size="m">
-           hh.ru
-          </Tag>
-        )}
+        <Htag tag='h2'>Вакансии - {page.category}</Htag>
+        <Tag color='red' size='m'>hh.ru</Tag>
       </div>
+      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
     </div>
   );
 };
