e224a63b9a4b788add1ac7252970de89978794b5
AntonVoronezh <pstut@mail.ru>
2022-01-27 17:51:43 +0300

    add Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,10 +2,10 @@ import { CardProps } from './Card.props';
 import styles from './Card.module.css';
 import cn from 'classnames';
 
-export const Card = ({ size = 'm', children, className, ...props }: CardProps): JSX.Element => {
+export const Card = ({ color = 'white', children, className, ...props }: CardProps): JSX.Element => {
   return (
-    <p className={cn(styles.p, className, { [styles.s]: size === 's', [styles.m]: size === 'm', [styles.l]: size === 'l' })} {...props}>
+    <div className={cn(styles.card, className, { [styles.blue]: color === 'blue'})} {...props}>
       {children}
-    </p>
+    </div>
   );
 };
