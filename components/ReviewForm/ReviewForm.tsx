a6519f5e52170ad8ae39fe70dc03a821d6c8b6a2
AntonVoronezh <pstut@mail.ru>
2022-02-02 17:10:33 +0300

    add err

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,16 +1,22 @@
+import { useForm, Controller } from 'react-hook-form';
+import cn from 'classnames';
+
 import { ReviewFormProps } from './ReviewForm.props';
 import styles from './ReviewForm.module.css';
 import CloseIcon from './close.svg';
-import cn from 'classnames';
 import { Input } from '../Input/Input';
 import { Rating } from '../Rating/Rating';
 import { Textarea } from '../Textarea/Textarea';
 import { Button } from '../Button/Button';
-import { useForm, Controller } from 'react-hook-form';
 import { IReviewForm } from './Revw.interface';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-  const { register, control, handleSubmit } = useForm<IReviewForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IReviewForm>();
 
   const onSubmit = (data: IReviewForm) => {
     console.log(data);
@@ -19,8 +25,13 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input {...register('name')} placeholder="Имя" />
-        <Input {...register('title')} placeholder="Заголовок отзыва" className={styles.title} />
+        <Input {...register('name', { required: { value: true, message: 'Заполните имя' } })} placeholder="Имя" error={errors.name} />
+        <Input
+          {...register('title', { required: { value: true, message: 'Заполните заголовок' } })}
+          placeholder="Заголовок отзыва"
+          className={styles.title}
+          error={errors.title}
+        />
         <div className={styles.rating}>
           <span>Оценка:</span>
           <Controller
@@ -29,7 +40,12 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
             render={({ field }) => <Rating isEditable rating={field.value} ref={field.ref} setRating={field.onChange} />}
           />
         </div>
-        <Textarea {...register('description')} placeholder="Текст отзыва" className={styles.description} />
+        <Textarea
+          {...register('description', { required: { value: true, message: 'Заполните описание' } })}
+          placeholder="Текст отзыва"
+          className={styles.description}
+          error={errors.description}
+        />
         <div className={styles.submit}>
           <Button appearance="primary">Отправить</Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
