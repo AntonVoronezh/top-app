e72c6c80aade25f34c5f55052380d2188dc84bf9
AntonVoronezh <pstut@mail.ru>
2022-02-04 17:26:01 +0300

    add aria

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -1,8 +1,9 @@
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
-import axios from 'axios';
-
-import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
+import { Head } from 'next/document';
 import { ParsedUrlQuery } from 'querystring';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+
+import axios from 'axios';
+import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
 import { ProductModel } from '../../interfaces/product.interface';
 import { withLayout } from '../../layout/Layout';
 import { MenuItem } from '../../interfaces/menu.interface';
@@ -11,7 +12,18 @@ import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-  return <TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+  return (
+    <>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name="description" content={page.metaDescription} />
+        <meta property="og:title" content={page.metaTitle} />
+        <meta property="og:description" content={page.metaDescription} />
+        <meta property="og:type" content="article" />
+      </Head>
+      <TopPageComponent firstCategory={firstCategory} page={page} products={products} />
+    </>
+  );
 }
 
 export default withLayout(TopPage);
